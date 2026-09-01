50094fe69285e02cd6fbc4e7d8232973310fc9a8
Ezekiel Marfil <ekel.marfil@gmail.com>
2026-09-01 16:47:13 +0800

    fixed gmail link

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,5 +1,5 @@
 export const socialLinks = [
   { label: "GitHub", href: "https://github.com/zkielmrfl", external: true },
   { label: "LinkedIn", href: "https://www.linkedin.com/in/ezekiel-marfil-683ab8380/", external: true },
-  { label: "Email", href: "mailto:ekel.marfil@gmail.com", external: false }
+  { label: "Email", href: "https://mail.google.com/mail/?view=cm&fs=1&to=ekel.marfil@gmail.com", external: true }
 ];
